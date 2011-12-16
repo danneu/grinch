4323a49d8016a48e2e092ee4b3fc3d16fefc401c
Dan Neumann <danrodneu@gmail.com>
2011-12-16 01:30:36 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grinch::Application.config.secret_token = 'eb14206ee8a97e67fb90aef298b604e90aa54992d89eee158945ff2dac613f55ed16cd6079f59fa8d0ecd6a6fdd450d411a5bb0f47c9ab493d5c1e23d7d93822'

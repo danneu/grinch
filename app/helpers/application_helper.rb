7c7e67a3fd4077ced66c6c3e4565eae683dbdfdb
Dan Neumann <danrodneu@gmail.com>
2011-12-21 15:46:00 -0600

    added posts styling, simple date formatter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def simple_date(date)
+    date.strftime("%d %b %Y")
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ActiveRecord::Base
-  attr_accessible :title, :body, :rendered_body
-  before_save :render_markdown
+  attr_accessible :title, :body
+  before_save :render_body
+  default_scope order('created_at DESC')
 
   private
 
-  def render_markdown 
+  def render_body
     require 'rdiscount'
     self.rendered_body = RDiscount.new(self.body).to_html
   end

eb7a660810be00a85c7279fae300c7ce78009934
Dan Neumann <danrodneu@gmail.com>
2011-12-22 06:11:17 -0600

    --amend

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
-  DEFAULT = Category.find_by_name("unfiled") || Category.create(name: "unfiled")
+  DEFAULT ||= Category.where(name: "unfiled") || Category.create(name: "unfiled")
   has_and_belongs_to_many :posts
 end

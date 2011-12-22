e0e454b063127fb7c1b9ffa16eb58d236ea0be1c
Dan Neumann <danrodneu@gmail.com>
2011-12-22 05:04:53 -0600

    improved active admin views

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,3 +1,8 @@
 ActiveAdmin.register Category do
-  
+  filter :name, as: :string
+  index do
+    column :id
+    column :name
+    default_actions
+  end
 end
--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,7 +1,13 @@
 ActiveAdmin.register Post do
   index do
     column :id
-    column :title
+    column "Title" do |post|
+      link_to post.title, admin_post_path(post)
+    end
+    column "Categories" do |post|
+      # post.categories.count # Why won't this work?
+      link_to post.categories.count, root_path
+    end
     column :created_at
     default_actions
   end

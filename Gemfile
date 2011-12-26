f34bb86ff42ddb461de854cc68436659bc40365e
Dan Neumann <danrodneu@gmail.com>
2011-12-26 00:08:38 -0600

    lots of active admin stuff removed. like routes, gems, migrations. might still be stuff left over

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.1.3'
 gem 'sqlite3'
-gem 'activeadmin'
 gem 'rdiscount'
 gem 'coderay'
 gem 'jquery-rails'
--- app/admin/categories.rb
+++
@@ -1,8 +0,0 @@
-ActiveAdmin.register Category do
-  filter :name, as: :string
-  index do
-    column :id
-    column :name
-    default_actions
-  end
-end

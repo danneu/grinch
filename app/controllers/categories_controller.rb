9be37a9408b08041d9aacb409191930d71753871
Dan Neumann <danrodneu@gmail.com>
2011-12-27 21:37:49 -0600

    categories edit & index added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,9 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
   def show
     @category = Category.find_by_slug!(params[:id])
     @posts = @category.posts
@@ -15,6 +20,25 @@ class CategoriesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+    @category = Category.find_by_slug!(params[:id])
+  end
+
+  def update
+    @category = Category.find_by_slug!(params[:id])
+    if @category.update_attributes(params[:category])
+      redirect_to categories_path, notice: "Category updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find_by_slug!(params[:id])
+    @category.destroy
+    redirect_to categories_path, notice: "Category deleted."
   end
 
 end

8c0362639a7cfe5d46e49e72fe5960e7cae36430
Dan Neumann <danrodneu@gmail.com>
2011-12-27 18:30:27 -0600

    added New Categories action/flow

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,17 @@ class CategoriesController < ApplicationController
     @posts = @category.posts
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      redirect_to root_path, notice: "Category created."
+    else
+      render :new
+    end
+  end
+
 end

ece1d07f6b21e536532ffcfbfaf890a214e374a3
Dan Neumann <danrodneu@gmail.com>
2011-12-27 21:17:05 -0600

    added posts#new form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,16 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by_slug!(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to root_path, notice: "Post created."
+    else
+    end
+  end
 end

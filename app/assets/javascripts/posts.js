fbbb3891ddc867336dc538260b12ce06828cd12e
Dan Neumann <danrodneu@gmail.com>
2012-04-03 01:26:10 -0500

    use pagedown for live preview as writing post markdown

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,14 @@
+$(document).ready(function () { 
+  var converter = new Markdown.Converter();
+  var updatePreview = function () {
+    var md = $('#post_body').val();
+    var html = converter.makeHtml(md);
+    $('#preview').html(html);
+  }
+  $('#post_body').live('keyup', function () {
+    updatePreview();
+  });
+  $('#post_body').live('click', function () {
+    updatePreview();
+  });
+});

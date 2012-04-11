2674003ffdd40a447cf97051eb4fac8f5fd2019c
Dan Neumann <danrodneu@gmail.com>
2012-04-11 00:29:39 -0500

    content controller only accepts local requests

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,8 +1,16 @@
 class ContentController < ApplicationController
+  before_filter :ensure_local
+
   def convert
     require 'redcarpet'
     extensions = {fenced_code_blocks: true}
     redcarpet = Redcarpet::Markdown.new(PygmentizeHTML, extensions)
     render text: redcarpet.render(URI::decode(params[:markdown]))
+  end
+
+  private
+  def ensure_local
+    #return false unless request.local?
+    render text: request.local?
   end
 end
